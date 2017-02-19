490b37ae7495dc6c5b164e1a6f1c75fa004821d1
Sunny Patel <Sunny@Sunnys-MacBook-Pro.local>
2017-02-19 10:23:39 +0530

    Commit Code - FlockSO - workingcode

---
+++ RESTApi.md
@@ -0,0 +1,24 @@
+https://api.stackexchange.com/2.2/search?order=desc&sort=votes&intitle=java%20how%20to%20read%20file&site=stackoverflow -- to get question 
+
+https://api.stackexchange.com/2.2/questions/20086784/answers?site=stackoverflow  -- to get the answerlist from the question. 
+
+
+https://api.stackexchange.com/2.2/answers/20086962?site=stackoverflow&filter=withbody -- to get the answer.
+
+
+https://api.stackexchange.com/2.2/questions/20086784/answers?site=stackoverflow&filter=withbody
+
+
+
+===========
+
+https://api.stackexchange.com/2.2/search?order=desc&sort=votes&intitle=java%20how%20to%20read%20file&site=stackoverflow -- to get questionID
+
+https://api.stackexchange.com/2.2/questions/20086784?site=stackoverflow&filter=withbody  -- to get the question body adn title
+
+
+https://api.stackexchange.com/2.2/questions/20086784/answers?site=stackoverflow&filter=withbody -- to get answers with body
+
+
+
+/SO ask java how to read file
---
+++ watcher.js
@@ -0,0 +1,84 @@
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
+// Keys are stringified userId-questionId pairs
+// Values are the last activity time
+var watcher = {};
+
+var url = 'https://api.stackexchange.com/2.2/';
+var questionAnswerUrlAppend = '?site=stackoverflow';
+
+function getLastActivityTime(questionId) {
+	var questionUrl = url + '/questions/' + questionId + questionAnswerUrlAppend;
+
+	console.log(questionUrl);
+
+	var lastActivityDate = 0;
+
+    client.get(questionUrl, function (data, response) {
+    	lastActivityDate = data.items[0].last_activity_date;
+        console.log("Last Activity Date - " + lastActivityDate); 
+    });
+
+    return lastActivityDate;
+}
+
+function checkForUpdates(callback) {
+	console.log("Checking for updates");
+	
+	console.log(watcher);
+
+	for(toWatchObj in watcher) {
+		currentTime = watcher[toWatchObj];
+	
+		console.log(toWatchObj);
+
+		var questionId = toWatchObj.split(",")[1];
+		var userId = toWatchObj.split(",")[0];
+
+		console.log("Question id: " + questionId);
+
+		var newTime = getLastActivityTime(questionId);
+
+		console.log("Current known last activity time: " + currentTime);
+
+		if (newTime + 10 > currentTime){
+			console.log("Sending message to bot")
+			event = {'userId': userId}
+			callback(userId, questionId);
+			watcher[questionId] =  newTime;
+		}
+	}
+}
+
+module.exports = {
+
+	getWatchList: function(userId){
+		console.log("Inside getWatchList");
+		var map = {};
+		for(toWatchObj in watcher) {
+			currentTime = watcher[toWatchObj];
+	
+			console.log(toWatchObj);
+
+			var questionId = toWatchObj.split(",")[1];
+			var userId = toWatchObj.split(",")[0];
+
+			if(!(userId in map)){
+				map[userId]=[];
+			}
+			map[userId].push(questionId);
+			
+		}
+		return map;
+	},
+	addWatcher: function (userId, questionId) {
+		console.log("Now watching for userId = " + userId + ", questionId = " + questionId);
+		watcher[[userId, questionId]] = getLastActivityTime(questionId);
+	},
+
+	startWatcher: function (callback) {
+		console.log("Starting watcher");
+		setInterval(function(){checkForUpdates(callback);}, 300000);
+	}
+};
